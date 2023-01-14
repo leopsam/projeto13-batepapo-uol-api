77a3c95739c6ac31d329380cbed979efad9d04b5
Leonardo Sampaio <leonardo.lps@outlook.com.br>
2023-01-14 17:21:18 -0300

    POST /participants
    não esta completo, falta retornar a msg de entrou na sala, para todos

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from 'cors'
 import { MongoClient, ObjectId } from "mongodb"
 import dotenv from "dotenv"
+import joi from 'joi'
 
 dotenv.config()
 const app = express()
@@ -11,6 +12,7 @@ app.use(express.json())
 const mongoCilent = new MongoClient(process.env.DATABASE_URL)
 let db
 
+//-----------------conexao banco mongo-----------------------
 try {
     await mongoCilent.connect()
     db = mongoCilent.db()
@@ -18,8 +20,39 @@ try {
 } catch (err) {
     console.log("erro no banco de dados");
 }
+//-------------------POST /participants------------------------
+app.post('/participants', async (req, res) => {
+    console.log("rodou post participants")
 
+	const participants = req.body;
+
+    const participantsSchema = joi.object({
+        name: joi.string().required(),
+      });
+
+    const validation = participantsSchema.validate(participants, { abortEarly: false });
+
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+	try {
+        const resp = await db.collection("participants").findOne({ name: participants.name }); // Erro
+
+        if (resp) return res.status(409).send("nome já está sendo utilizado")
+
+        await db.collection("participants").insertOne({ name: participants.name, lastStatus: Date.now()});
+
+        return res.sendStatus(201);
+
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+})
+//-------------------GET /participants-------------------------
 app.get("/participants", (req, res) => { 
+    console.log("rodou get participants")
     db.collection("participants").find().toArray()
     .then(dados => {
       return res.send(dados)
@@ -35,8 +68,7 @@ app.get("/participants", (req, res) => {
         return res.status(500).send(err.message);
     }*/
   });
-
-
+//-------------------porta do servidor-------------------------
 app.listen(5000, () => {
 	console.log("Servidor rodando!")
 })
